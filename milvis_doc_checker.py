77556e5431abcde1f80d86812533d0f1f5038d94
Lutkin Wang <yamasite@qq.com>
2019-09-20 18:12:54 +0800

    Added a class to check link status.

--- milvis_doc_checker.py
+++ milvis_doc_checker.py
@@ -6,6 +6,7 @@ import urllib.request
 from lxml import etree
 from pathlib import Path
 import os
+import requests
 
 
 class GetURLsFromSitemap(object):
@@ -15,20 +16,24 @@ class GetURLsFromSitemap(object):
         self.url = url
 
 
-    def get_url_list(url):
+    def get_url_list(self, url):
 
         file_name = "outputlinks.txt"
 
+        # Gets the sitemap file per the URL
         def get_sitemap(url):
+
             try:
                 response = urllib.request.urlopen(url, data=None)
+                xml_code = response.read()
+                # print(type(xml_code))
+                # Take URLs with the following format: <xhtml:link rel="alternate" hreflang="en" href="https://www.milvus.io/docs/en/aboutmilvus/overview"/>
+                xml_root = etree.HTML(xml_code)
+                # print(etree.tostring(xml_root))
             except urllib.request.URLError as e:
+                xml_root = etree.HTML("")
                 print(e)
-            xml_code = response.read()
-            # print(type(xml_code))
-            # Take URLs with the following format: <xhtml:link rel="alternate" hreflang="en" href="https://www.milvus.io/docs/en/aboutmilvus/overview"/>
-            xml_root = etree.HTML(xml_code)
-            # print(etree.tostring(xml_root))
+
             return xml_root
 
         xml_root = get_sitemap(url)
@@ -51,7 +56,6 @@ class GetURLsFromSitemap(object):
         #print(links)
 
         # Writes the URL list to a text file
-
         text_file = Path(file_name)
         if text_file.is_file():
             os.remove(file_name)
@@ -60,5 +64,59 @@ class GetURLsFromSitemap(object):
             for link in links:
              f.write(link + "\n")
 
+        return file_name
 
-GetURLsFromSitemap.get_url_list("https://milvus.io/sitemap.xml")
+
+
+class CheckLinkStatus(object):
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def check_link_status(self, file_name):
+
+        report_name="link_report.txt"
+
+        with open(file_name,'r') as f:
+            links = f.readlines()
+
+        text_file = Path(report_name)
+        if text_file.is_file():
+            os.remove(report_name)
+
+        for link in links:
+            try:
+                print(link)
+                print(type(link))
+                r = requests.head(link)
+                status_code = r.status_code
+
+                """
+                Informational responses (100–199),
+                Successful responses (200–299),
+                Redirects (300–399),
+                Client errors (400–499),
+                and Server errors (500–599).
+                """
+
+                with open(report_name,'a') as f:
+                    if status_code in range(100-199):
+                        f.write("Status code: " + str(status_code) + " " + str(link) + " has no errors." + "\n")
+                    if status_code in range(200,299):
+                        f.write("Status code: " + str(status_code) + " " + str(link) + " has no errors." + "\n")
+                    elif status_code in range(300,399):
+                        f.write("Status code: " + str(status_code) + " " + str(link) + " has a redirect." + "\n")
+                    elif status_code in range(400,599):
+                        f.write("Status code: " + str(status_code) + " " + str(link) + " has errors." + "\n")
+
+
+            except requests.ConnectionError:
+                print("Failed to connect.")
+
+
+SitemapURLMilvus = GetURLsFromSitemap("https://milvus.io/sitemap.xml")
+SitemapURLMilvus.get_url_list("https://milvus.io/sitemap.xml")
+CheckLinkStatusMilvus = CheckLinkStatus("outputlinks.txt")
+CheckLinkStatusMilvus.check_link_status("outputlinks.txt")
+
+
